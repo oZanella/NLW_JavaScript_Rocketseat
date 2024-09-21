0c552eb3d407de8ebea335d290206aface710020
oZanella <henriquezanella19@gmail.com>
2024-09-21 14:46:47 -0300

    Estrutuad condicional switch inserida

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const start = () =>{
-  let count = 0
-  while(count < 10){
-    console.log(count)
-    count++ 
+const start = () => {
+
+  while (true){
+    let opcao = 'Sair'
+    switch(opcao) {
+      case 'cadastrar':
+        console.log('vamos cadastrar')
+        break
+      case 'listar':
+        console.log('vamos listar')
+        break
+      case 'sair':
+        return
+    }
   }
 }
 
