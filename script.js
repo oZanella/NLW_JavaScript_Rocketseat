48b6f5c04dcc9a93dbfbd070ea411ecfaa6e61b9
oZanella <henriquezanella19@gmail.com>
2024-09-22 23:02:46 -0300

    Verificar sobre listar metas por não exibi

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
-  value: 'agua todo dia',
+  value: 'Agua todo dia',
   checked: false,
 }
 
-let metas = [meta]
+let metas = [ meta ]
 
 const cadMeta = async () => {
   const meta = await input({ message: 'Digite sua meta:' })
@@ -20,7 +20,29 @@ const cadMeta = async () => {
   metas.push(
     { value: meta, checked: false }
   )
+}
 
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message: "Use as setas para mudar de meta, espaço para marcar/desmarcar e enter para confirmar",
+    choices: [...metas],
+    instructions: false,
+  })
+
+  if (respostas.length == 0) {
+    console.log('Nenhuma meta selecionada')
+    return
+  }
+
+  respostas.forEach((resposta) => {
+    const meta = metas.find((n) => {
+      return m.value == resposta
+    })
+
+    meta.checked = true
+  })
+
+  console.log('Meta(s) foram atualizada(s)')
 }
 
 const start = async () => {
@@ -51,7 +73,7 @@ const start = async () => {
         console.log(metas)
         break
       case 'listar':
-        console.log('vamos listar')
+        await listarMetas
         break
       case 'sair':
         console.log('Até a próxima! :)')
